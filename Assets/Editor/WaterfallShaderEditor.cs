f46ed9ccaa1e505e849ac7df1629377d7a464094
Bazzan <sebake01@gmail.com>
2020-12-14 12:34:30 +0100

    Initial commit

---
+++ Assets/Editor/WaterfallShaderEditor.cs
@@ -0,0 +1,177 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WaterfallShader))]
+public class WaterfallShaderEditor : Editor
+{
+
+
+    private WaterfallShader waterfallShader;
+    private bool isReady = false;
+    private GameObject selected;
+
+    private WaterfallShaderEditor waterfallShaderEditor;
+    private new SerializedObject serializedObject;
+
+    private SerializedProperty flowSpeed;
+    private SerializedProperty offset;
+    private SerializedProperty tiling;
+    private SerializedProperty blackColor;
+    private SerializedProperty darkenColor;
+    private SerializedProperty whiteColor;
+    private SerializedProperty waterfallMasks;
+
+
+    private GUILayoutOption[] colorBoxLayout;
+    private void OnEnable()
+    {
+        isReady = false;
+        selected = Selection.activeGameObject;
+        if (!selected) return;
+
+        if (!waterfallShader)
+            waterfallShader = selected.GetComponent<WaterfallShader>();
+
+        serializedObject = new SerializedObject(waterfallShader);
+        GetProperties();
+        EditorUtility.SetDirty(target);
+
+
+        //SetColorBoxLayoutOption();
+
+
+        isReady = true;
+    }
+
+    //private void SetColorBoxLayoutOption()
+    //{
+    //    colorBoxLayout[0] = GUILayout.Width(10);
+    //    colorBoxLayout[1] = GUILayout.Height(10);
+
+    //}
+
+
+
+    public override void OnInspectorGUI()
+    {
+        if (!isReady) return;
+
+        serializedObject.Update();
+        ShowWaterfallUI();
+        Undo.RecordObject(target, "Component");
+
+        serializedObject.ApplyModifiedProperties();
+        waterfallShader.SetProperties();
+    }
+
+
+    private void GetProperties()
+    {
+        Debug.Log("running");
+        waterfallShader.GetProperties();
+
+        flowSpeed = serializedObject.FindProperty("FlowSpeed");
+        tiling = serializedObject.FindProperty("Tiling");
+        offset = serializedObject.FindProperty("Offset");
+        blackColor = serializedObject.FindProperty("WhiteColor");
+        whiteColor = serializedObject.FindProperty("BlackColor");
+        darkenColor = serializedObject.FindProperty("DarkenColor");
+        waterfallMasks = serializedObject.FindProperty("WaterfallMasks");
+    }
+
+
+    private void ShowWaterfallUI()
+    {
+        if (!selected) return;
+        TopUI();
+        SettingsUI();
+
+
+    }
+
+    private void TopUI()
+    {
+        EditorGUILayout.Space();
+        using (new GUILayout.HorizontalScope())
+        {
+            GUILayout.Label("My first waterfalls shade ui ", new GUIStyle(EditorStyles.boldLabel)
+            {
+                alignment = TextAnchor.MiddleLeft,
+                wordWrap = true,
+                fontSize = 11,
+            });
+        }
+        EditorGUILayout.Space();
+
+        FontStyle originalFontStyle = EditorStyles.label.fontStyle;
+        EditorStyles.label.fontStyle = FontStyle.Bold;
+
+        AddSliderSettings("Adjusts the speed streaming water", flowSpeed, 0f, 10f);
+        AddSliderSettings("Adjusts the size of tiles", tiling, 0f, 15f);
+        AddSliderSettings("Offsets the texture", offset, 0f, 1f);
+
+        ExplanationBox("Colors");
+        AddColorSettings(blackColor, "Black Color");
+        AddColorSettings(whiteColor, "White Color");
+        AddColorSettings(darkenColor, "Darken Color");
+
+
+
+
+        DetiledExplanationBox("R channel: Main Color Mask,\n" +
+                              "G channel: Highlight Pattern,\n" +
+                              "B channel: Variation Map");
+
+        EditorGUILayout.ObjectField(waterfallMasks, typeof(Texture2D));
+    }
+    private void SettingsUI()
+    {
+
+        
+    }
+    private void AddColorSettings(/*string explanation,*/ SerializedProperty property, string colorName)
+    {
+
+        //DetiledExplanationBox(explanation);
+        using (new GUILayout.HorizontalScope())
+        {
+            EditorGUILayout.PropertyField(property, new GUIContent(colorName) );
+        }
+    }
+
+
+    private void AddSliderSettings(string explanation,SerializedProperty property ,float minFloat, float maxFloat)
+    {
+        DetiledExplanationBox(explanation);
+        using (new GUILayout.HorizontalScope())
+        {
+            EditorGUILayout.Slider(property, minFloat, maxFloat);
+        }
+    }
+
+
+    private void DetiledExplanationBox(string inputText)
+    {
+        using (new GUILayout.HorizontalScope())
+        {
+            GUILayout.Label(inputText, new GUIStyle(EditorStyles.helpBox)
+            {
+                alignment = TextAnchor.MiddleLeft,
+                wordWrap = true,
+                fontSize = 12,
+            });
+        }
+    }
+    private void ExplanationBox(string inputText)
+    {
+        using (new GUILayout.HorizontalScope())
+        {
+            GUILayout.Label(inputText, new GUIStyle(EditorStyles.helpBox)
+            {
+                alignment = TextAnchor.MiddleLeft,
+                wordWrap = true,
+                fontSize = 14,
+            });
+        }
+    }
+}
---
+++ Assets/scripts/WaterfallShader.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+#endif
+
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshRenderer))]
+public class WaterfallShader : MonoBehaviour
+{
+    public Material material;
+    public Renderer meshRenderer;
+    public Shader Shader;
+
+
+    public float FlowSpeed;
+    public float Tiling;
+    public float Offset;
+
+    public Color WhiteColor;
+    public Color DarkenColor;
+    public Color BlackColor;
+
+    public Texture2D WaterfallMasks;
+
+    private void OnEnable()
+    {
+        if (!meshRenderer)
+            meshRenderer = GetComponent<Renderer>();
+        if (!material)
+            material = meshRenderer.sharedMaterial;
+        GetProperties();
+        SetProperties();
+    }
+
+    public void GetProperties()
+    {
+        FlowSpeed = material.GetFloat("_FlowSpeed");
+        Tiling = material.GetFloat("_Tiling");
+        Offset = material.GetFloat("_Offset");
+
+        WhiteColor = material.GetColor("_WhiteColor");
+        BlackColor = material.GetColor("_BlackColor");
+        DarkenColor = material.GetColor("_DarkenColor");
+
+        WaterfallMasks = material.GetTexture("_WaterfallMasks") as Texture2D;
+    }
+
+    public void SetProperties()
+    {
+        material.SetFloat("_FlowSpeed", FlowSpeed);
+        material.SetFloat("_Tiling", Tiling);
+        material.SetFloat("_Offset", Offset);
+
+        material.SetColor("_BlackColor", BlackColor);
+        material.SetColor("_WhiteColor", WhiteColor);
+        material.SetColor("_DarkenColor", DarkenColor);
+
+        material.SetTexture("_WaterfallMask", WaterfallMasks);
+    }
+
+
+
+}
